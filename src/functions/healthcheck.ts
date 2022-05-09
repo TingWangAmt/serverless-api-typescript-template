788b3a45578eaf8780cf09c43f6a0f1882664305
TingWangAmt <t.wang@am-tech.co.jp>
2022-05-09 18:02:46 +0900

    add localhost

--- src/functions/healthcheck.ts
+++ src/functions/healthcheck.ts
@@ -1,12 +1,30 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from '../libs/api-gateway';
 
-module.exports.healthcheck = async () => {
-    return {
-        body: 'OK',
-        headers: {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "https://master.d2w7htrom5yxv0.amplifyapp.com",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        },
-        statusCode: 200,
-    };
-}
+const schema = {
+    type: "object",
+    properties: {
+      name: { type: 'string' }
+    },
+    required: ['name']
+  };
+
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> =  async (event) => {
+    const origin = event.headers['origin'];
+    const resOriginHeader = ["https://master.d2w7htrom5yxv0.amplifyapp.com/", "http://localhost:3000/"];
+    const responseHeaders = {
+        'Access-Control-Allow-Origin': '',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+      };
+    if (!!origin && resOriginHeader.includes(origin)) {
+       responseHeaders['Access-Control-Allow-Origin'] = origin
+    }
+      return {
+          body: 'OK',
+          headers: responseHeaders,
+          statusCode: 200,
+      };
+  }
+
+module.exports.healthcheck = hello;
