b4c0c4006dcd346186cb2f65d0e202dc1ae2a3ca
TingWangAmt <t.wang@am-tech.co.jp>
2022-05-09 17:02:35 +0900

    add some

--- src/functions/healthcheck.ts
+++ src/functions/healthcheck.ts
@@ -2,6 +2,11 @@
 module.exports.healthcheck = async () => {
     return {
         body: 'OK',
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "https://master.d2w7htrom5yxv0.amplifyapp.com",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         statusCode: 200,
     };
 }
