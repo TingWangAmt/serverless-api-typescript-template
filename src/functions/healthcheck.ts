72bc8b46bcbd2acb1c923314aa248e32917c1313
TingWangAmt <t.wang@am-tech.co.jp>
2022-05-11 12:13:04 +0900

    add dynamodb

--- src/functions/healthcheck.ts
+++ src/functions/healthcheck.ts
@@ -1,18 +1,13 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '../libs/api-gateway';
+import { Handler } from 'aws-lambda';
+import * as AWS from 'aws-sdk'; 
 
-const schema = {
-    type: "object",
-    properties: {
-      name: { type: 'string' }
-    },
-    required: ['name']
-  };
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> =  async (event) => {
+const hello: Handler = (event, context, callback) => {
     const origin = event.headers['origin'];
     const resOriginHeader = ["https://master.d2w7htrom5yxv0.amplifyapp.com", "http://localhost:3000"];
     const responseHeaders = {
-        'Access-Control-Allow-Origin': '',
+        'Access-Control-Allow-Origin': 'https://master.d2w7htrom5yxv0.amplifyapp.com',
         'Access-Control-Allow-Credentials': true,
         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
         'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
@@ -20,11 +15,52 @@ const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> =  async (event) 
     if (!!origin && resOriginHeader.includes(origin)) {
        responseHeaders['Access-Control-Allow-Origin'] = origin
     }
-      return {
-          body: 'OK',
-          headers: responseHeaders,
-          statusCode: 200,
+
+    const params ={
+      TableName: "test",
+      Item: {name:Date.now().toString(), age:33}
+    };
+
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the todo item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+
+    dynamoDb.scan({
+      TableName: "test1",
+    }, (error, result) => {
+      if (error) {
+        console.error(error);
+        callback(null, {
+          statusCode: error.statusCode || 501,
+          headers: { 'Content-Type': 'text/plain' },
+          body: error.message,
+        });
+        return;
+      }
+
+      // create a response
+      const response = {
+        statusCode: 200,
+        headers: responseHeaders,
+        body: JSON.stringify(result.Items),
       };
+      callback(null, response);
+    });
   }
 
 module.exports.healthcheck = hello;
